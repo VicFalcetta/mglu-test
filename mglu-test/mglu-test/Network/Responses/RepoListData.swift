7f46d12c8f7034844aebacb32d8884f91ff94ee9
Victor Falcetta do Nascimento <vic.falcetis@gmail.com>
2022-05-18 16:38:17 -0300

    Add request for repo list

---
+++ mglu-test/mglu-test/Network/Responses/RepoListData.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+struct RepoListData: Decodable, Equatable {
+    let items: [SingleRepoData]
+}
+
+struct SingleRepoData: Decodable, Equatable {
+    let name: String
+    let description: String
+    let owner: OwnerData
+    let stargazersCount: Int
+    let forksCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case description
+        case owner
+        case stargazersCount = "stargazers_count"
+        case forksCount = "forks_count"
+    }
+}
+
+struct OwnerData: Decodable, Equatable {
+    let login: String
+    let avatar: String
+    
+    enum CodingKeys: String, CodingKey {
+        case login
+        case avatar = "avatar_url"
+    }
+}
---
+++ mglu-test/mglu-test/Network/Service/GitHubAppService.swift
@@ -0,0 +1,39 @@
+import Foundation
+import Alamofire
+
+enum GitHubAppService {
+    case repoList(page: Int)
+    case pullList(owner: String, repo: String)
+}
+
+extension GitHubAppService: Request {
+    var path: String {
+        switch self {
+        case .repoList:
+            return "/search/repositories"
+        case .pullList(let owner, let repo):
+            return "/repos/\(owner)/\(repo)/pulls"
+        }
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var encoding: ParameterEncoding {
+        JSONEncoding()
+    }
+    
+    var parameters: [String : Any]? {
+        switch self {
+        case .repoList(let page):
+            return [
+                "language": "Swift",
+                "sort": "stars",
+                "page": "\(page)"
+            ]
+        default:
+            return nil
+        }
+    }
+}
